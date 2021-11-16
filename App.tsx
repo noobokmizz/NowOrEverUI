6b8b18d0223c462bcd0618c70ee90cbb41da4d42
nooblette <min9hyuk@gmail.com>
2021-11-17 00:33:26 +0900

    patch mh 1

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@
 import 'react-native-gesture-handler';
 
 // Import React and Component
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 // Import Navigators from React Navigation
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -17,8 +17,52 @@ import TabNavigation from './Screen/Tab';
 //import Bucket from './Screen/Bucket';
 
 const Stack = createStackNavigator();
-export const localhost='192.168.238.63'; //학교
+//export const localhost='192.168.238.63'; //학교
 //export const localhost='192.168.35.57'; //집
+export const localhost='15.164.123.22'; // public server ip 
+
+/* Switch Navigator for those screens which needs to be switched only once
+  and we don't want to switch back once we switch from them to the next one */
+function App() {
+
+  useEffect(() => {
+    console.log('waiting firebase message');
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      console.log("arrive message!");
+    });
+
+    return unsubscribe;
+  }, []);
+
+ return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen">
+
+        {/* SplashScreen which will come once for 5 Seconds*/ }
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          // Hiding header for Splash Screen
+          options={{headerShown: false}}
+        />
+        {/* Auth Navigator which includer Login Signup will come once*/ }
+        <Stack.Screen
+          name="Auth"
+          component={Auth}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+         name="TabNavigation"
+         options={{headerShown: false}}
+         component={TabNavigation} />
+        {/* Navigation Drawer as a landing page */}
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
 
 const Auth = () => {
   // Stack Navigator for Login and Sign up Screen
@@ -45,48 +89,6 @@ const Auth = () => {
         }}
       />
     </Stack.Navigator>
-  );
-};
-
-/* Switch Navigator for those screens which needs to be switched only once
-  and we don't want to switch back once we switch from them to the next one */
-const App = () => {
-
-  useEffect(() => {
-    console.log('waiting firebase message');
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-      console.log("arrive message!");
-    });
-
-    return unsubscribe;
-  }, []);
-  
- return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="SplashScreen">
-        
-        {/* SplashScreen which will come once for 5 Seconds*/ }
-        <Stack.Screen
-          name="SplashScreen"
-          component={SplashScreen}
-          // Hiding header for Splash Screen
-          options={{headerShown: false}}
-        />
-        {/* Auth Navigator which includer Login Signup will come once*/ }
-        <Stack.Screen
-          name="Auth"
-          component={Auth}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-         name="TabNavigation"
-         options={{headerShown: false}}
-         component={TabNavigation} />
-        {/* Navigation Drawer as a landing page */}
-        
-      </Stack.Navigator>
-    </NavigationContainer>
   );
 };
 
